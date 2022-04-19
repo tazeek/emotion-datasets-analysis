585fbad40feb6835a9e953fd14e2ab1ef9f6209d
tazeek <tazeek.rakib@gmail.com>
2022-04-19 15:29:01 +0800

    Description for downloader added

--- EmoryExtractor.py
+++ EmoryExtractor.py
@@ -4,8 +4,17 @@
 class EmoryExtractor:
 
     def __init__(self) -> None:
+        self._files_list = []
         self._total_files = 10
         self._file_name = "friends_season_"
         self._url_json = "https://raw.githubusercontent.com/emorynlp/character-mining/master/json/"
+
+        return None
+
+    def download_files(self):
+        """
+            Downloads the JSON files, converts to a dictionary, and stores in a list.
+            This is done for all 10 files (file count as at 19/04/2022)
+        """
 
         return None
