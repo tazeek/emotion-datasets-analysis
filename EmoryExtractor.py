79e1633e86926689e3a8c5574c222eab8fbb316b
tazeek <tazeek.rakib@gmail.com>
2022-04-19 15:24:26 +0800

    Attributes of files added

--- EmoryExtractor.py
+++ EmoryExtractor.py
@@ -1,6 +1,11 @@
 # Repository of file: https://github.com/emorynlp/character-mining/tree/master/json
+# Example of raw file: https://raw.githubusercontent.com/emorynlp/character-mining/master/json/friends_season_01.json
 
 class EmoryExtractor:
 
     def __init__(self) -> None:
-        pass
+        self._total_files = 10
+        self._file_name = "friends_season_"
+        self._url_json = "https://raw.githubusercontent.com/emorynlp/character-mining/master/json/"
+
+        return None
