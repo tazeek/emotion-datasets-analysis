da80fc3d83d2600815f314a2bbb7ffe977bb6122
tazeek <tazeek.rakib@gmail.com>
2022-04-20 00:11:57 +0800

    Downloader code added

--- dailydialog/DailydialogExtractor.py
+++ dailydialog/DailydialogExtractor.py
@@ -1,7 +1,19 @@
 # Website link: http://yanran.li/dailydialog
 # Dataset: http://yanran.li/files/ijcnlp_dailydialog.zip
+from io import BytesIO
+import requests, zipfile
 
 class DailydialogExtractor:
 
     def __init__(self) -> None:
-        pass
+        self._file_url = "http://yanran.li/files/ijcnlp_dailydialog.zip"
+    
+    def download_files(self) -> None:
+
+        req = requests.get(self._file_url)
+        print(req.content)
+
+        zipfile= zipfile.ZipFile(BytesIO(req.content))
+        zipfile.extractall(r'data/')
+
+        return None
