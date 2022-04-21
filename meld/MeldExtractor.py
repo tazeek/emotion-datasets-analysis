b769456a9c546d0ff1b3eecfd4c6c9cfdde715a2
tazeek <tazeek.rakib@gmail.com>
2022-04-21 17:01:38 +0800

    File loaded successfully

--- meld/MeldExtractor.py
+++ meld/MeldExtractor.py
@@ -1,10 +1,12 @@
 # Dataset: https://raw.githubusercontent.com/declare-lab/MELD/master/data/MELD/datasets.yaml
+import requests
 
 class MeldExtractor:
 
     def __init__(self) -> None:
         self._dataset_url = "https://raw.githubusercontent.com/declare-lab/MELD/master/data/MELD/datasets.yaml"
 
-    def download_data(self) -> None:
-        pass
+    def download_file(self) -> None:
+        downloaded_file = requests.get(self._dataset_url).json()
 
+        return None
