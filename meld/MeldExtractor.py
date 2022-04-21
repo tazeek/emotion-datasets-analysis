283ad6e37cfec457a5fe053ae46c52184102917b
tazeek <tazeek.rakib@gmail.com>
2022-04-21 16:50:08 +0800

    MELD file being implemented

---
+++ meld/MeldExtractor.py
@@ -0,0 +1,10 @@
+# Dataset: https://raw.githubusercontent.com/declare-lab/MELD/master/data/MELD/datasets.yaml
+
+class MeldExtractor:
+
+    def __init__(self) -> None:
+        self._dataset_url = "https://raw.githubusercontent.com/declare-lab/MELD/master/data/MELD/datasets.yaml"
+
+    def download_data(self) -> None:
+        pass
+
