d93ed81399e911e44de58d142a8ee857a7418b03
tazeek <tazeek.rakib@gmail.com>
2022-04-25 15:15:33 +0800

    Reccon extractor file to be used

---
+++ reccon/RecconExtractor.py
@@ -0,0 +1,7 @@
+# Dataset location: https://github.com/declare-lab/RECCON/tree/main/data/original_annotation
+# Raw data: https://github.com/declare-lab/RECCON/blob/main/data/original_annotation/dailydialog_test.json
+
+class RecconExtractor:
+
+    def __init__(self) -> None:
+        pass
