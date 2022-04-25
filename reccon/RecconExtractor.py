36c7d3e0b8afe0dab96613bc41367e9bb7ebb80a
tazeek <tazeek.rakib@gmail.com>
2022-04-25 23:41:14 +0800

    Files saved successfully

--- reccon/RecconExtractor.py
+++ reccon/RecconExtractor.py
@@ -1,5 +1,7 @@
 # Dataset location: https://github.com/declare-lab/RECCON/tree/main/data/original_annotation
 # Raw data: https://raw.githubusercontent.com/declare-lab/RECCON/main/data/original_annotation/dailydialog_test.json
+import requests
+import json
 
 class RecconExtractor:
 
@@ -8,16 +10,26 @@ class RecconExtractor:
             'dailydialog_train.json', 'dailydialog_valid.json'#, 'iemocap_test.json'
         ]
 
+        self._files_list = []
         self._json_url = 'https://raw.githubusercontent.com/declare-lab/RECCON/main/data/original_annotation/'
 
     def _prepare_url(self, file_name) -> str:
 
         return self._json_url + file_name
 
+    def _save_file(self, filename) -> None:
+        with open(filename, 'w') as file:
+            json.dump(self._files_list, file, indent=4)
+
+        return None
+
     def download_files(self) -> None:
 
         for file in self._files_list:
             url = self._prepare_url(file)
-            print(url)
+            extracted_json = requests.get(url).json()
+            self._files_list.append(extracted_json)
+
+        self._save_file('reccon/data_level0.json')
 
         return None
