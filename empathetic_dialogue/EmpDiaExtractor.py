6f046d55b35bc32227e158ed5941df027ac057cf
tazeek <tazeek.rakib@gmail.com>
2022-04-27 14:51:53 +0800

    Almost extracted using tarfile

--- empathetic_dialogue/EmpDiaExtractor.py
+++ empathetic_dialogue/EmpDiaExtractor.py
@@ -2,26 +2,28 @@
 # File location: https://dl.fbaipublicfiles.com/parlai/empatheticdialogues/empatheticdialogues.tar.gz
 
 from io import BytesIO
-import requests, zipfile
+import requests, tarfile
 
 class EmpDiaExtractor:
 
     def __init__(self) -> None:
         self._file_url = "https://dl.fbaipublicfiles.com/parlai/empatheticdialogues/empatheticdialogues.tar.gz"
-        self._data_dir = "empathetic-dialogue/data/"
+        self._data_dir = "empathetic_dialogue/data/"
+    
+    def _filter_files_zip(self, filename) -> bool:
+        return filename.endswith(('csv'))
 
     def _rename_file(self, file_fragment):
-        return self._data_dir + file_fragment.filename.split('/')[-1]
+        return self._data_dir + file_fragment.name.split('/')[-1]
 
     def download_files(self) -> None:
 
-        req = requests.get(self._file_url)
+        req = requests.get(self._file_url, stream=True)
+        file_list = tarfile.open(fileobj=req.raw, mode="r|gz")
 
-        zip_file= zipfile.ZipFile(BytesIO(req.content))
-
-        for file in zip_file.infolist():
-            if self._filter_files_zip(file.filename):
-                file.filename = self._rename_file(file)
-                zip_file.extract(file)
+        for file in file_list.getmembers():
+            if self._filter_files_zip(file.name):
+                file.name = self._rename_file(file)
+                file_list.extractfile(file)
 
         return None
