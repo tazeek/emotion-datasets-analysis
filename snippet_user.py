eb91816350f97e7f4d833daacbc714116bf935da
Tazeek <tazeek.rakib@gmail.com>
2022-04-27 14:17:34 +0800

    Only use extractor methods

--- snippet_user.py
+++ snippet_user.py
@@ -3,15 +3,11 @@ from dailydialog.DailydialogExtractor import DailydialogExtractor
 from meld.MeldExtractor import MeldExtractor
 from reccon.RecconExtractor import RecconExtractor
 
-#emory_extractor = EmoryExtractor()
-#emory_extractor.download_files()
-#emory_extractor.load_file()
+emory_extractor = EmoryExtractor()
+emory_extractor.download_files()
 
-#daily_extractor = DailydialogExtractor()
-#daily_extractor.download_files()
-
-#meld_extractor = MeldExtractor()
-#meld_extractor.download_file()
+daily_extractor = DailydialogExtractor()
+daily_extractor.download_files()
 
 reccon_extractor = RecconExtractor()
 reccon_extractor.download_files()
