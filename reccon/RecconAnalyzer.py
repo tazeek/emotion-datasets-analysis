07b8f05e3aafa3542b7482a780ee8c924b5061e5
tazeek <tazeek.rakib@gmail.com>
2022-05-28 14:46:55 +0800

    Find number of tokens per dialogue

--- reccon/RecconAnalyzer.py
+++ reccon/RecconAnalyzer.py
@@ -17,20 +17,27 @@ class RecconAnalyzer:
         # For finding the types of cause, for emotions (per utterance)
         self._cause_type_counter = {}
 
+        # Find the number of tokens (per utterance)
+        self._token_counts = []
+
         # Find the number of utterances (per dialogue)
         self._utt_per_diag_counter = []
 
-        # Find the number of tokens (per utterance)
-        self._token_counts = []
+        # Find the number of tokens (per dialogue)
+        self._token_counts_per_diag = []
 
         return None
 
     def _load_file(self) -> json:
         return json.load(open('data_level0.json',encoding="utf-8"))
 
-    def _update_utterance_counter(self, utterance: str) -> None:
-        utter_tokens = utterance.split(" ")
-        self._token_counts += [len(utter_tokens)]
+    def _update_tokens_per_diag(self, total_tokens: int) -> None:
+      self._token_counts_per_diag += [total_tokens]
+      
+      return None  
+
+    def _update_utt_counter(self, utter_len: int) -> None:
+        self._token_counts += [utter_len]
 
         return None
 
@@ -50,14 +57,21 @@ class RecconAnalyzer:
 
         return None
     
-    def _parse_utterance_dict(self, utt_dict: dict) -> None:
+    def _parse_utterance_dict(self, utt_dict: dict, total_tokens: int) -> None:
         emotion = utt_dict.get("emotion", None)
         cause_type_list = utt_dict.get("type", ['empty'])
         utterance = utt_dict.get("utterance", "")
 
+        utterance_tokens = utterance.split(" ")
+        utter_len = len(utterance_tokens)
+
+        # Count number of tokens
+        total_tokens += len(utterance_tokens)
+
+        # Update respective functions (per utterance)
         self._update_emotion_counter(emotion)
         self._update_type_counter(cause_type_list)
-        self._update_utt_counter(utterance)
+        self._update_utt_counter(utter_len)
 
         return None
 
@@ -76,6 +90,9 @@ class RecconAnalyzer:
     def fetch_token_counter(self) -> list:
         return self._token_counts
 
+    def fetch_token_counts_diag(self) -> list:
+        return self._token_counts_per_diag
+
     def fetch_partition_file(self, part: str) -> json:
         index =  self._dataset_division[part]
         return self._json_file[index]
@@ -84,10 +101,13 @@ class RecconAnalyzer:
         
         for key in file.keys():
             dialogue_set = file[key][0]
-
-            self._update_utter_diag_counter(dialogue_set)
+            total_tokens = 0
 
             for utterance_dict in dialogue_set:
-                self._parse_utterance_dict(utterance_dict)
+                self._parse_utterance_dict(utterance_dict, total_tokens)
+
+            # Update respective functions (per dialogue)
+            self._update_utter_diag_counter(dialogue_set)
+            self._update_tokens_per_diag(total_tokens)
         
         return None
