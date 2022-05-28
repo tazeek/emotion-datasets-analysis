91f2a62fe97f011472c0f8b61cc63123a13e6ab9
tazeek <tazeek.rakib@gmail.com>
2022-05-28 13:47:32 +0800

    Dataset partitioning done

--- reccon/RecconAnalyzer.py
+++ reccon/RecconAnalyzer.py
@@ -1,4 +1,18 @@
+import json
+
 class RecconAnalyzer:
 
     def __init__(self):
-        pass
+        self._json_file =self. _load_file()
+
+        self._dataset_division = {
+            'test': 0,
+            'train': 1,
+            'valid': 2,
+        }
+
+    def _load_file(self):
+        return json.load(open('data_level0.json',encoding="utf-8"))
+
+    def fetch_raw_json(self) -> json:
+        return self._json_file
