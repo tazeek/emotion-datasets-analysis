75309797da2feecda31d9f47a4c75f5dfaec794e
tazeek <tazeek.rakib@gmail.com>
2022-05-29 22:57:24 +0800

    Additional functions added

--- meld/MeldAnalyzer.py
+++ meld/MeldAnalyzer.py
@@ -15,8 +15,14 @@ class MeldAnalyzer:
         self._json_file = json.load(open('data_level0.json', encoding="utf8"))
         return None
 
-    def extract_partitions(self) -> list:
+    def extract_partitions_keys(self) -> list:
         return list(self._json_file.keys())
 
     def fetch_raw_data(self) -> json:
         return self._json_file
+
+    def partition_file(self, key: str) -> json:
+        return self._json_file[key]
+
+    def analyze_partition(self, partition: json) -> None:
+        pass
