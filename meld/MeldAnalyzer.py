d22898e8f33c1d1828ed136b554e40fa4574097f
tazeek <tazeek.rakib@gmail.com>
2022-05-29 22:52:47 +0800

    Load and partition data properly

--- meld/MeldAnalyzer.py
+++ meld/MeldAnalyzer.py
@@ -2,12 +2,21 @@ import json
 
 class MeldAnalyzer:
 
-    def __init__(self):
+    def __init__(self) -> None:
+
+        # Raw data
         self._json_file = None
+
+        self._load_data()
+
+        return None
 
     def _load_data(self) -> None:
         self._json_file = json.load(open('data_level0.json', encoding="utf8"))
         return None
 
-    def extract_partitions(self):
-        return self._json_file.keys()
+    def extract_partitions(self) -> list:
+        return list(self._json_file.keys())
+
+    def fetch_raw_data(self) -> json:
+        return self._json_file
