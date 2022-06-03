163b139abb990c33cd4c5577ecca9f9351ef79f3
tazeek <tazeek.rakib@gmail.com>
2022-06-03 11:52:06 +0800

    Mapper functions added

--- dailydialog/DailydialogAnalyzer.py
+++ dailydialog/DailydialogAnalyzer.py
@@ -23,3 +23,21 @@ class DailydialogAnalyzer:
                 label_dict[index] = line.split()
                 
         return label_dict
+
+    def topic_mapping() -> dict:
+        return {
+            '1': 'Ordinary Life', '2': 'School Life', '3': 'Culture & Education',
+            '4': 'Attitude & Emotion', '5': 'Relationship', '6': 'Tourism' , '7': 'Health', 
+            '8': 'Work', '9': 'Politics', '10': 'Finance'
+        }
+
+    def dialog_mapping() -> dict:
+        return {
+            '1': 'inform', '2': 'question', '3': 'directive', '4': 'commissive'
+        }
+
+    def emotion_mapping() -> dict:
+        return { 
+            '0': 'no emotion', '1': 'anger', '2': 'disgust', '3': 'fear', 
+            '4': 'happiness', '5': 'sadness', '6': 'surprise'
+        }
