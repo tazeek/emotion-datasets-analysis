12cc019fee7b4013e3573fdf513f99ea3b5b2591
tazeek <tazeek.rakib@gmail.com>
2022-06-03 11:50:51 +0800

    Read labels from files

--- dailydialog/DailydialogAnalyzer.py
+++ dailydialog/DailydialogAnalyzer.py
@@ -3,7 +3,17 @@
 class DailydialogAnalyzer:
 
     def __init__(self):
-        pass
+        
+        # For reading emotion labels
+        self._emotion_labels_dict = self._read_label_file('dialogues_emotion.txt')
+
+        # For reading annotation labels
+        self._annotation_labels_dict = self._read_label_file('dialogues_act.txt')
+
+        # For reading topic labels
+        self._topic_labels_dict = self._read_label_file('dialogues_topic.txt')
+
+        return
 
     def _read_label_file(self, label_file: str) -> dict:
         label_dict = {}
