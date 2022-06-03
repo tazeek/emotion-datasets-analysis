287c236d9aafc7b28b68bb0c9989a59a9eadb050
tazeek <tazeek.rakib@gmail.com>
2022-06-03 11:48:15 +0800

    Templte function for reading files added

--- dailydialog/DailydialogAnalyzer.py
+++ dailydialog/DailydialogAnalyzer.py
@@ -4,3 +4,12 @@ class DailydialogAnalyzer:
 
     def __init__(self):
         pass
+
+    def _read_label_file(self, label_file: str) -> dict:
+        label_dict = {}
+    
+        with open(f"data\{label_file}") as file:
+            for index, line in enumerate(file.readlines()):
+                label_dict[index] = line.split()
+                
+        return label_dict
