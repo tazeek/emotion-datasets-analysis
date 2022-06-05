f22700bd16239942cb75aebabe2561ff473c35af
tazeek <tazeek.rakib@gmail.com>
2022-06-05 22:20:15 +0800

    Season-based analysis added

--- emorynlp/EmoryAnalyzer.py
+++ emorynlp/EmoryAnalyzer.py
@@ -7,7 +7,15 @@ class EmoryAnalyzer:
         self._json_file = self._load_file
         pass
 
+    def _season_analysis(self):
+        pass
+
     def _load_file(self):
         filename = 'data_level0.json'
         f = open(filename)
         return json.load(f)
+
+    def _begin_analysis(self):
+
+        for season in self._json_file:
+            pass
