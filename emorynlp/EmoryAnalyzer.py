688c63c737d85023a48a31b14b7809a1be8b2385
tazeek <tazeek.rakib@gmail.com>
2022-06-11 21:51:55 +0800

    Parser functions activated

--- emorynlp/EmoryAnalyzer.py
+++ emorynlp/EmoryAnalyzer.py
@@ -5,6 +5,13 @@ class EmoryAnalyzer:
     def __init__(self):
 
         self._json_file = self._load_file
+
+        self._episodes_per_season = []
+
+        self._scenes_per_episode = []
+
+        self._utterances_per_scene = []
+
         pass
 
     def _load_file(self):
@@ -15,16 +22,40 @@ class EmoryAnalyzer:
     def _begin_analysis(self):
 
         for season in self._json_file:
-            pass
+            season_number = season['season_id']
+            episodes = season['episodes']
+
+            # Find number of episodes per season
+            self._episodes_per_season += [len(episodes)]
+            
+            self._episode_analysis(episodes)
+
+        return None
 
     def _season_analysis(self):
         pass
 
-    def _episode_analysis(self):
-        pass
+    def _episode_analysis(self, episodes: list):
+        
+        for episode in episodes:
+            scenes = episode['scenes']
 
-    def _scenes_analysis(self):
-        pass
+            # Find number of scenes per episode
+            self._scenes_per_episode += [len(scenes)]
+
+            self._scenes_analysis(scenes)
+
+
+        return None
+
+    def _scenes_analysis(self, scenes: list):
+        
+        for scene in scenes:
+            utterances = scene['utterances']
+
+            self._utterances_per_scene += [len(utterances)]
+
+        return None
 
     def _utterance_analysis(self):
         pass
