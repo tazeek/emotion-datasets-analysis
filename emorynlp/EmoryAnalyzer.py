d0ab1b03e86f241cfc792a7e8331d0abd20aa352
tazeek <tazeek.rakib@gmail.com>
2022-06-11 21:56:18 +0800

    Clean up code

--- emorynlp/EmoryAnalyzer.py
+++ emorynlp/EmoryAnalyzer.py
@@ -6,12 +6,6 @@ class EmoryAnalyzer:
 
         self._json_file = self._load_file
 
-        self._episodes_per_season = []
-
-        self._scenes_per_episode = []
-
-        self._utterances_per_scene = []
-
         pass
 
     def _load_file(self):
@@ -22,26 +16,16 @@ class EmoryAnalyzer:
     def _begin_analysis(self):
 
         for season in self._json_file:
-            season_number = season['season_id']
             episodes = season['episodes']
-
-            # Find number of episodes per season
-            self._episodes_per_season += [len(episodes)]
             
             self._episode_analysis(episodes)
 
         return None
 
-    def _season_analysis(self):
-        pass
-
     def _episode_analysis(self, episodes: list):
         
         for episode in episodes:
             scenes = episode['scenes']
-
-            # Find number of scenes per episode
-            self._scenes_per_episode += [len(scenes)]
 
             self._scenes_analysis(scenes)
 
@@ -52,8 +36,6 @@ class EmoryAnalyzer:
         
         for scene in scenes:
             utterances = scene['utterances']
-
-            self._utterances_per_scene += [len(utterances)]
 
         return None
 
