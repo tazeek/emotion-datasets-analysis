3dc57ea52cc8dab8d8fe874d67ef1a553b848b31
tazeek <tazeek.rakib@gmail.com>
2022-06-15 22:31:50 +0800

    Dictionary increment function added

--- reccon/RecconAnalyzer.py
+++ reccon/RecconAnalyzer.py
@@ -137,6 +137,14 @@ class RecconAnalyzer:
         self._emotion_shift_counts += [emotion_shifts_count]
         return None
 
+    def _increment_dictionary_turn_cause(self, difference_list: list) -> None:
+
+        for difference in difference_list:
+            self._emotion_turn_cause[difference] = \
+                self._emotion_turn_cause.get(difference, 0) + 1
+
+        return None
+
     def _update_emotion_turn_cause_counts(self, dialog_list: list) -> None:
 
         helper_lambda = lambda utt: utt.get('expanded emotion cause evidence', [])
@@ -161,10 +169,7 @@ class RecconAnalyzer:
 
             # Normal turn counts
             cause_distance_list = [turn_number - cause_number for cause_number in cause_list]
-            print(turn_number)
-            print(cause_list)
-            print(cause_distance_list)
-            print("\n")
+            self._increment_dictionary_turn_cause(cause_distance_list)
         return None
 
     def _parse_dialog_dict(self, dialog_list: list) -> None:
